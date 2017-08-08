fa7ca957d74488b27958ea77ee3933d68b8a4b42
Frederick Akalin <akalin@gmail.com>
2017-08-08 02:12:13 -0500

    Add SSSE3 implementation of standardToAltMap

--- gf2p16/slice_amd64.go
+++ gf2p16/slice_amd64.go
@@ -25,3 +25,13 @@ func mulSliceUnsafe(cEntry *mulTableEntry, in, out []T)
 
 //go:noescape
 func mulAndAddSliceUnsafe(cEntry *mulTableEntry, in, out []T)
+
+// standardToAltMapSSSE3Unsafe sets
+//
+//   *outLow  = in0[0,2,4,6,8,10,12,14] . in1[0,2,4,6,8,10,12,14]
+//   *outHigh = in0[1,3,5,7,9,11,13,15] . in1[1,3,5,7,9,11,13,15],
+//
+// where . denotes concatenation.
+//
+//go:noescape
+func standardToAltMapSSSE3Unsafe(in0, in1, outLow, outHigh *[16]byte)
--- gf2p16/slice_amd64.s
+++ gf2p16/slice_amd64.s
@@ -45,3 +45,62 @@ loop:
 	JLT     $0, loop
 
 	RET
+
+// func standardToAltMapSSSE3Unsafe(in0, in1, outLow, outHigh *[16]byte)
+TEXT ·standardToAltMapSSSE3Unsafe(SB), NOSPLIT, $0
+	// X0 = *in0
+	MOVQ  in0+0(FP), AX
+	MOVOU (AX), X0
+
+	// X1 = *in1
+	MOVQ  in1+8(FP), AX
+	MOVOU (AX), X1
+
+	// All 128-bit words are written in big-endian form below.
+
+	// Set X4 = 00ff00ff:00ff00ff:00ff00ff:00ff00ff, clobbering X5.
+	MOVQ   $0xff, BX
+	MOVQ   BX, X4
+	PXOR   X5, X5
+	PSHUFB X5, X4
+	PSRLW  $8, X4
+
+	// Letting each digit represent a single byte, if
+	//
+	//   *in0 = X0 = 1234:5678:9abc:defg
+	//   *in1 = X1 = hijk:lmno:pqrs:tuvw,
+	//
+	// set
+	//
+	//   X0 = 0103:0507:090b:0d0f
+	//   X2 = 0204:0608:0a0c:0e0g,
+	MOVO  X0, X2
+	PSRLW $8, X0
+	PAND  X4, X2
+
+	// and
+	//
+	//   X1 = 0h0j:0l0n:0p0r:0t0v
+	//   X3 = 0i0k:0m0o:0q0s:0u0w.
+	MOVO  X1, X3
+	PSRLW $8, X1
+	PAND  X4, X3
+
+	// Then set X0 to the low bytes of each nibble of X1.X0,
+	// and X2 to the low bytes of each nibble of X3.X2, where .
+	// denotes concatenation, i.e.
+	//
+	//   X0 = hjln:prtv:1357:9bdf
+	//   X2 = ikmo:qsuw:2468:aceg.
+	PACKUSWB X1, X0
+	PACKUSWB X3, X2
+
+	// *outLow = X2
+	MOVQ  outLow+16(FP), AX
+	MOVOU X2, (AX)
+
+	// *outHigh = X0
+	MOVQ  outHigh+24(FP), AX
+	MOVOU X0, (AX)
+
+	RET
