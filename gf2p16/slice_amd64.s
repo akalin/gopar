ec8130747fb7ca5461ec1c452b835c4f530c3cdb
Frederick Akalin <akalin@gmail.com>
2017-08-08 09:18:47 -0500

    Define STANDARD_TO_ALT_MAP_SSSE3 macro and use it
    Also define SET_CONV_MASK_SSSE3 macro and use it.

--- gf2p16/slice_amd64.s
+++ gf2p16/slice_amd64.s
@@ -46,6 +46,56 @@ loop:
 
 	RET
 
+// All 128-bit words are written in big-endian form below.
+
+// Sets out = 00ff00ff:00ff00ff:00ff00ff:00ff00ff, clobbering tmp and
+// tmpx. out and tmpx should be 128-bit registers, i.e. beginning with X,
+// and tmp should be a general purpose register, e.g. AX, BX.
+#define SET_CONV_MASK_SSSE3(out, tmp, tmpx) \
+	MOVQ   $0xff, tmp \
+	MOVQ   tmp, out   \
+	PXOR   tmpx, tmpx \
+	PSHUFB tmpx, out  \
+	PSRLW  $8, out
+
+// All arguments should be 128-bit registers, i.e. beginning with X.
+// convMask should be set to 00ff00ff:00ff00ff:00ff00ff:00ff00ff.
+//
+// Sets in0 to outHigh, and clobbers in1 and tmp.
+//
+// Letting each digit represent a single byte, if
+//
+//   in0 = 1234:5678:9abc:defg
+//   in1 = hijk:lmno:pqrs:tuvw,
+//
+// set (first clause)
+//
+//   in0    = 0103:0507:090b:0d0f
+//   outLow = 0204:0608:0a0c:0e0g,
+//
+// and (second clause)
+//
+//   in1 = 0h0j:0l0n:0p0r:0t0v
+//   tmp = 0i0k:0m0o:0q0s:0u0w.
+//
+// Then set in0 to the low bytes of each nibble of in1.in0,
+// and outLow to the low bytes of each nibble of tmp.outLow, where .
+// denotes concatenation, i.e.
+//
+//   in0 = outHigh = hjln:prtv:1357:9bdf
+//         outLow  = ikmo:qsuw:2468:aceg.
+#define STANDARD_TO_ALT_MAP_SSSE3(in0, in1, convMask, outLow, tmp) \
+	MOVO     in0, outLow      \
+	PSRLW    $8, in0          \
+	PAND     convMask, outLow \
+	                          \
+	MOVO     in1, tmp         \
+	PSRLW    $8, in1          \
+	PAND     convMask, tmp    \
+	                          \
+	PACKUSWB in1, in0         \
+	PACKUSWB tmp, outLow
+
 // func standardToAltMapSSSE3Unsafe(in0, in1, outLow, outHigh *[16]byte)
 TEXT ·standardToAltMapSSSE3Unsafe(SB), NOSPLIT, $0
 	// X0 = *in0
@@ -56,44 +106,8 @@ TEXT ·standardToAltMapSSSE3Unsafe(SB), NOSPLIT, $0
 	MOVQ  in1+8(FP), AX
 	MOVOU (AX), X1
 
-	// All 128-bit words are written in big-endian form below.
-
-	// Set X4 = 00ff00ff:00ff00ff:00ff00ff:00ff00ff, clobbering X5.
-	MOVQ   $0xff, BX
-	MOVQ   BX, X4
-	PXOR   X5, X5
-	PSHUFB X5, X4
-	PSRLW  $8, X4
-
-	// Letting each digit represent a single byte, if
-	//
-	//   *in0 = X0 = 1234:5678:9abc:defg
-	//   *in1 = X1 = hijk:lmno:pqrs:tuvw,
-	//
-	// set
-	//
-	//   X0 = 0103:0507:090b:0d0f
-	//   X2 = 0204:0608:0a0c:0e0g,
-	MOVO  X0, X2
-	PSRLW $8, X0
-	PAND  X4, X2
-
-	// and
-	//
-	//   X1 = 0h0j:0l0n:0p0r:0t0v
-	//   X3 = 0i0k:0m0o:0q0s:0u0w.
-	MOVO  X1, X3
-	PSRLW $8, X1
-	PAND  X4, X3
-
-	// Then set X0 to the low bytes of each nibble of X1.X0,
-	// and X2 to the low bytes of each nibble of X3.X2, where .
-	// denotes concatenation, i.e.
-	//
-	//   X0 = hjln:prtv:1357:9bdf
-	//   X2 = ikmo:qsuw:2468:aceg.
-	PACKUSWB X1, X0
-	PACKUSWB X3, X2
+	SET_CONV_MASK_SSSE3(X4, BX, X5)
+	STANDARD_TO_ALT_MAP_SSSE3(X0, X1, X4, X2, X3)
 
 	// *outLow = X2
 	MOVQ  outLow+16(FP), AX
