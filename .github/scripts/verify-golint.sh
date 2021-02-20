ad988b5fb0ae7e98f23c47c396dfce059633d2eb
Frederick Akalin <akalin@gmail.com>
2021-02-20 02:46:13 -0600

    Make verify-golint.sh more robust

--- .github/scripts/verify-golint.sh
+++ .github/scripts/verify-golint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-golint_output=$(golint $1)
+golint_output=$(golint $1 2>&1)
+golint_exit_code=$?
 
-if [[ -n "${golint_output}" ]]; then
+if [[ "$golint_exit_code" -ne 0 ]]; then
+    echo "golint exited with $golint_exit_code"
+    echo "${golint_output}"
+    exit $golint_exit_code
+elif [[ -n "${golint_output}" ]]; then
+    echo "golint found errors"
     echo "${golint_output}"
     exit 1
 else
