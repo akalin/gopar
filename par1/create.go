153cd8ef15cac38f8f1a47b4b6ebed08d2b21d76
Frederick Akalin <akalin@gmail.com>
2021-03-21 01:21:35 -0500

    Add CreateDelegate with delegate function and use it

--- par1/create.go
+++ par1/create.go
@@ -2,7 +2,6 @@ package par1
 
 import (
 	"errors"
-	"fmt"
 	"path"
 	"path/filepath"
 )
@@ -11,25 +10,35 @@ import (
 // CreateOptions.NumParityFiles if the latter is <= 0.
 const NumParityFilesDefault = 3
 
-// DoNothingEncoderDelegate is an implementation of EncoderDelegate
-// that does nothing for all methods.
-type DoNothingEncoderDelegate struct{}
-
-// OnDataFileLoad implements the EncoderDelegate interface.
-func (DoNothingEncoderDelegate) OnDataFileLoad(i, n int, path string, byteCount int, err error) {}
-
-// OnVolumeFileWrite implements the EncoderDelegate interface.
-func (DoNothingEncoderDelegate) OnVolumeFileWrite(i, n int, path string, dataByteCount, byteCount int, err error) {
+// CreateDelegate extends EncoderDelegate with another delegate
+// function.
+type CreateDelegate interface {
+	EncoderDelegate
+	OnFilesNotAllInSameDir()
 }
+
+// DoNothingCreateDelegate is an implementation of CreateDelegate that
+// does nothing for all methods.
+type DoNothingCreateDelegate struct{}
+
+// OnDataFileLoad implements the CreateDelegate interface.
+func (DoNothingCreateDelegate) OnDataFileLoad(i, n int, path string, byteCount int, err error) {}
+
+// OnVolumeFileWrite implements the CreateDelegate interface.
+func (DoNothingCreateDelegate) OnVolumeFileWrite(i, n int, path string, dataByteCount, byteCount int, err error) {
+}
+
+// OnFilesNotAllInSameDir implements the CreateDelegate interface.
+func (DoNothingCreateDelegate) OnFilesNotAllInSameDir() {}
 
 // CreateOptions holds all the options for Create.
 type CreateOptions struct {
 	// The number of parity files to create. If <= 0,
 	// NumParityFilesDefault is used.
 	NumParityFiles int
-	// The EncoderDelegate to use. If nil,
-	// DoNothingEncoderDelegate is used.
-	EncoderDelegate EncoderDelegate
+	// The CreateDelegate to use. If nil, DoNothingCreateDelegate
+	// is used.
+	CreateDelegate CreateDelegate
 }
 
 // Create a par file for the given file paths at parPath with the
@@ -53,22 +62,21 @@ func create(fileIO fileIO, parPath string, filePaths []string, options CreateOpt
 		numParityFiles = NumParityFilesDefault
 	}
 
-	delegate := options.EncoderDelegate
+	delegate := options.CreateDelegate
 	if delegate == nil {
-		delegate = DoNothingEncoderDelegate{}
+		delegate = DoNothingCreateDelegate{}
 	}
 
 	parDir := filepath.Dir(parPath)
-	allFilesInSameDir := true
+	filesAllInSameDir := true
 	for _, p := range filePaths {
 		if filepath.Dir(p) != parDir {
-			allFilesInSameDir = false
+			filesAllInSameDir = false
 			break
 		}
 	}
-	// TODO: Use a delegate method for this.
-	if !allFilesInSameDir {
-		fmt.Printf("Warning: PAR and data files not all in the same directory, which a decoder will expect\n")
+	if !filesAllInSameDir {
+		delegate.OnFilesNotAllInSameDir()
 	}
 
 	encoder, err := newEncoder(fileIO, delegate, filePaths, numParityFiles)
