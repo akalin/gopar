9530b9bbd25167c341799120b7676dc5708f771f
Frederick Akalin <akalin@gmail.com>
2021-05-29 01:54:38 -0500

    Add new ReadStream interface and GetReadStream to fs.FS
    Add implementations for defaultFS, MemFS, and testFS.

--- fs/fs.go
+++ fs/fs.go
@@ -1,11 +1,44 @@
 package fs
 
+import "io"
+
+// ReadStream defines the interface for streaming file reads. This is
+// usually implemented by *os.File, but there might be other
+// implementations for testing.
+type ReadStream interface {
+	io.ReadCloser
+	// TODO: Once streaming is used everywhere, evaluate whether
+	// we still need this function.
+	ByteCount() int64
+}
+
+type readCloserStream struct {
+	io.ReadCloser
+	byteCount int64
+}
+
+func (rcs readCloserStream) ByteCount() int64 {
+	return rcs.byteCount
+}
+
+// ReadCloserToStream makes and returns a ReadStream out of the given
+// ReadCloser and byte count.
+func ReadCloserToStream(readCloser io.ReadCloser, byteCount int64) ReadStream {
+	return readCloserStream{readCloser, byteCount}
+}
+
 // FS is the interface used by the par1 and par2 packages to the
 // filesystem. Most code uses DefaultFS, but tests may use other
 // implementations.
 type FS interface {
 	// ReadFile should behave like ioutil.ReadFile.
 	ReadFile(path string) ([]byte, error)
+	// GetReadStream returns a ReadStream to read the file at the
+	// given path.
+	//
+	// Implementations must guarantee that exactly one of the
+	// returned ReadStream and error is non-nil.
+	GetReadStream(path string) (ReadStream, error)
 	// FindWithPrefixAndSuffix should behave like calling
 	// filepath.Glob with prefix + "*" + suffix.
 	FindWithPrefixAndSuffix(prefix, suffix string) ([]string, error)
--- testfs/testfs.go
+++ testfs/testfs.go
@@ -1,10 +1,48 @@
 package testfs
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/akalin/gopar/fs"
 )
+
+type testReadStream struct {
+	t    *testing.T
+	path string
+	rs   fs.ReadStream
+}
+
+func (trs testReadStream) Read(p []byte) (n int, err error) {
+	trs.t.Helper()
+	defer func() {
+		trs.t.Helper()
+		trs.t.Logf("Read(%q, %d bytes) => (%d bytes, %v)", trs.path, len(p), n, err)
+	}()
+	return trs.rs.Read(p)
+}
+
+func (trs testReadStream) Close() (err error) {
+	trs.t.Helper()
+	defer func() {
+		trs.t.Helper()
+		trs.t.Logf("Close(%q) => (%v)", trs.path, err)
+	}()
+	return trs.rs.Close()
+}
+
+func (trs testReadStream) ByteCount() (byteCount int64) {
+	trs.t.Helper()
+	defer func() {
+		trs.t.Helper()
+		trs.t.Logf("ByteCount(%q) => (%d)", trs.path, byteCount)
+	}()
+	return trs.rs.ByteCount()
+}
+
+func (trs testReadStream) String() string {
+	return fmt.Sprintf("testReadStream{%q}", trs.path)
+}
 
 type testFS struct {
 	t  *testing.T
@@ -24,6 +62,20 @@ func (fs testFS) ReadFile(path string) (data []byte, err error) {
 		fs.t.Logf("ReadFile(%q) => (%d bytes, %v)", path, len(data), err)
 	}()
 	return fs.fs.ReadFile(path)
+}
+
+func (fs testFS) GetReadStream(path string) (readStream fs.ReadStream, err error) {
+	fs.t.Helper()
+	defer func() {
+		fs.t.Helper()
+		fs.t.Logf("GetReadStream(%q) => (%v, %v)", path, readStream, err)
+	}()
+
+	readStream, err = fs.fs.GetReadStream(path)
+	if readStream != nil {
+		readStream = testReadStream{fs.t, path, readStream}
+	}
+	return readStream, err
 }
 
 func (fs testFS) FindWithPrefixAndSuffix(prefix, suffix string) (matches []string, err error) {
