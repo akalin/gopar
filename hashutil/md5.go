8c55b0892a51104b46357fa527df1b603c04b6e9
Frederick Akalin <akalin@gmail.com>
2021-05-29 23:45:30 -0500

    Add MD5HashWith16k utility function

--- hashutil/md5.go
+++ hashutil/md5.go
@@ -11,3 +11,14 @@ func MD5Hash16k(data []byte) [md5.Size]byte {
 	}
 	return md5.Sum(data[:16*1024])
 }
+
+// MD5HashWith16k returns the MD5 hash of the input, as well as the
+// MD5 hash of the first 16k bytes of the input, or the MD5 hash of
+// the full input again if it has fewer than 16k bytes.
+func MD5HashWith16k(data []byte) (hash [md5.Size]byte, hash16k [md5.Size]byte) {
+	hash = md5.Sum(data)
+	if len(data) < 16*1024 {
+		return hash, hash
+	}
+	return hash, md5.Sum(data[:16*1024])
+}
--- hashutil/md5_test.go
+++ hashutil/md5_test.go
@@ -8,20 +8,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var testInputs = [][]byte{
+	{},
+	bytes.Repeat([]byte{0x1}, 100),
+	bytes.Repeat([]byte{0x2}, 16*1024-1),
+	bytes.Repeat([]byte{0x3}, 16*1024),
+	bytes.Repeat([]byte{0x4}, 16*1024+1),
+}
+
 func TestMD5Hash16k(t *testing.T) {
-	inputs := [][]byte{
-		{},
-		bytes.Repeat([]byte{0x1}, 100),
-		bytes.Repeat([]byte{0x2}, 16*1024-1),
-		bytes.Repeat([]byte{0x3}, 16*1024),
-		bytes.Repeat([]byte{0x4}, 16*1024+1),
-	}
-	for _, input := range inputs {
+	for _, input := range testInputs {
 		hash16k := MD5Hash16k(input)
 		if len(input) < 16*1024 {
 			require.Equal(t, md5.Sum(input), hash16k)
 		} else {
 			require.Equal(t, md5.Sum(input[:16*1024]), hash16k)
 		}
+	}
+}
+
+func TestMD5HashWith16k(t *testing.T) {
+	for _, input := range testInputs {
+		hash, hash16k := MD5HashWith16k(input)
+		require.Equal(t, md5.Sum(input), hash)
+		require.Equal(t, MD5Hash16k(input), hash16k)
 	}
 }
