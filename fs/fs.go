633a663d0e3d4593a7e2c98bae22be823db27108
Frederick Akalin <akalin@gmail.com>
2021-05-30 01:45:13 -0500

    Use GetReadStream in par1.Encoder
    Add fs.ReadAndClose helper function that will eventually go away.
    
    Also add helper readStrict and readFullEOF functions, with tests.

--- fs/fs.go
+++ fs/fs.go
@@ -1,6 +1,9 @@
 package fs
 
-import "io"
+import (
+	"errors"
+	"io"
+)
 
 // ReadStream defines the interface for streaming file reads. This is
 // usually implemented by *os.File, but there might be other
@@ -51,4 +54,88 @@ type FS interface {
 	FindWithPrefixAndSuffix(prefix, suffix string) ([]string, error)
 	// WriteFile should behave like ioutil.WriteFile.
 	WriteFile(path string, data []byte) error
+}
+
+// readStrict checks that len(buf) != 0, calls r.Read(buf), and checks
+// that the return value isn't 0, nil.
+func readStrict(r io.Reader, buf []byte) (n int, err error) {
+	if len(buf) == 0 {
+		return 0, errors.New("len(buf) == 0 unexpectedly in readStrict")
+	}
+	n, err = r.Read(buf)
+	if n == 0 && err == nil {
+		return n, errors.New("r.Read() returned 0, nil")
+	}
+	return n, err
+}
+
+// readFullEOF is like io.ReadFull, except that it:
+//
+//   - requires len(buf) to be non-zero,
+//   - calls readStrict instead,
+//   - doesn't drop the error even if the buffer is completely filled,
+//     except when the error is EOF,
+//   - if the buffer is completely filled, checks that the next read from
+//     the reader triggers an EOF.
+func readFullEOF(r io.Reader, buf []byte) (n int, err error) {
+	if len(buf) == 0 {
+		return 0, errors.New("len(buf) == 0 unexpectedly in readFullEOF")
+	}
+	for n < len(buf) && err == nil {
+		var nn int
+		nn, err = readStrict(r, buf[n:])
+		n += nn
+	}
+	if n < len(buf) {
+		// Loop termination condition guarantees err != nil.
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return n, err
+	}
+	// Now we know that n >= len(buf) (really n == len(buf)), so
+	// we just have to examine err.
+	if err == io.EOF {
+		err = nil
+	}
+	if err != nil {
+		return n, err
+	}
+
+	// Now we know that we filled up buf with no error (or EOF),
+	// so now we just make sure that we're actually at EOF.
+	var singleByte [1]byte
+	_, err = readStrict(r, singleByte[:])
+	if err == io.EOF {
+		err = nil
+	}
+	return n, err
+}
+
+// ReadAndClose reads all the data in the given io.ReadCloser into a
+// buffer and returns it, closing it in all cases.
+//
+// If err == nil, the returned buffer will never be nil, even if it
+// has length 0.
+//
+// TODO: Make this function unnecessary.
+func ReadAndClose(readStream ReadStream) (data []byte, err error) {
+	defer func() {
+		closeErr := readStream.Close()
+		if err == nil {
+			err = closeErr
+		}
+	}()
+	byteCount := readStream.ByteCount()
+	if int64(int(byteCount)) != byteCount {
+		return nil, errors.New("file too big to read into memory")
+	}
+	data = make([]byte, byteCount)
+	if len(data) > 0 {
+		_, err = readFullEOF(readStream, data)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return data, nil
 }
